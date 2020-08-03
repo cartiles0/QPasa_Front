3c4404e2248c07c4d605db5a91ad12a3a8e93a31
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-08-03 16:13:51 +0100

    changed navBar without v-if

--- components/navBar.vue
+++ components/navBar.vue
@@ -9,7 +9,7 @@
             <v-icon>mdi-menu</v-icon>
           </v-btn>
         </template>
-        <v-list v-if="userLogged === true">
+        <!-- <v-list v-if="userLogged === true">
           <v-list-item
             v-for="(item, index) in items"
             :key="index"
@@ -32,6 +32,101 @@
             <v-list-item-title>
               <v-icon>{{ item.logo }}</v-icon>
               {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list> -->
+        <v-list v-if="userLogged === true">
+          <v-list-item
+            to="signup"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-account</v-icon>
+              Create Account
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="login"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-account</v-icon>
+              Log In
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-magnify</v-icon>
+              Search
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-flag</v-icon>
+              Categories
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            active-class="deep-purple--text text--accent-4"
+            @click="logout()"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-exit-to-app</v-icon>
+              Log Out
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <!--  -->
+        <v-list v-else>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-account</v-icon>
+              My Profile
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-calendar-plus</v-icon>
+              Create Event
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-magnify</v-icon>
+              Search
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            to="createEvent"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-flag</v-icon>
+              Categories
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            active-class="deep-purple--text text--accent-4"
+            @click="logout()"
+          >
+            <v-list-item-title>
+              <v-icon>mdi-exit-to-app</v-icon>
+              Log Out
             </v-list-item-title>
           </v-list-item>
         </v-list>
@@ -66,7 +161,10 @@ export default {
       ],
     }
   },
-  method: {
+  methods: {
+    logout() {
+      localStorage.clear()
+    },
     logged() {
       if (!localStorage.getItem('token')) {
         return (this.userLogged = false)
