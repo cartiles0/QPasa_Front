b8ccb44d2f7461257554721818a3383910f37f18
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-08-06 18:37:13 +0100

    tagChips work

--- components/tagChips.vue
+++ components/tagChips.vue
@@ -1,21 +1,41 @@
 <template>
   <div class="text-center">
-    <v-chip class="ma-2" color="success" outlined>
+    <v-chip
+      class="ma-2"
+      color="success"
+      :to="`/events/category/Gastronomy`"
+      outlined
+    >
       <v-icon left>mdi-pound</v-icon>
       Gastronomía
     </v-chip>
 
-    <v-chip class="ma-2" color="primary" outlined>
+    <v-chip
+      class="ma-2"
+      color="primary"
+      :to="`/events/category/Concerts`"
+      outlined
+    >
       <v-icon left>mdi-pound</v-icon>
       Conciertos
     </v-chip>
 
-    <v-chip class="ma-2" color="deep-purple accent-4" outlined>
+    <v-chip
+      class="ma-2"
+      color="deep-purple accent-4"
+      :to="`/events/category/Sports`"
+      outlined
+    >
       <v-icon left>mdi-pound</v-icon>
-      Festival
+      Sports
     </v-chip>
 
-    <v-chip class="ma-2" color="indigo darken-3" outlined>
+    <v-chip
+      class="ma-2"
+      color="indigo darken-3"
+      :to="`/events/category/Expo%2FFairs`"
+      outlined
+    >
       <v-icon left>mdi-pound</v-icon>
       Expo/Ferias
     </v-chip>
