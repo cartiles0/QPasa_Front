c0b23d72bb317c3e66778c59346da4cc675d53a8
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-08-10 07:00:13 +0100

    added edit User feature

--- pages/users/me/index.vue
+++ pages/users/me/index.vue
@@ -1,90 +1,202 @@
 <template>
-  <v-card class="mx-auto my-5 justify-center elevation-0" max-width="1500px">
-    <v-list-item>
-      <v-avatar>
-        <img :src="user.photo" :alt="user.name" />
-      </v-avatar>
-      <v-list-item-content class="ml-4">
-        <v-list-item-title class="py-0">
-          {{ user.username }}
+  <div>
+    <v-card class="mx-auto my-5 justify-center elevation-0" max-width="1500px">
+      <div class="editUserForm">
+        <v-row justify="end">
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon small color="#666666">mdi-account-edit</v-icon>
+              <v-btn
+                class="pl-0"
+                color="#666666"
+                small
+                dark
+                v-bind="attrs"
+                text
+                v-on="on"
+              >
+                Edit Profile
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Edit Profile</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        v-model="editProfile.name"
+                        type="name"
+                        label="Name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        v-model="editProfile.lastName"
+                        type="Last Name"
+                        label="Last Name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="editProfile.email"
+                        type="email"
+                        label="Email"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-select
+                        v-model="editProfile.areaPreference"
+                        :items="[
+                          'Las Palmas de Gran Canaria',
+                          'Telde',
+                          'Agaete',
+                          'Agüimes',
+                          'Artenara',
+                          'Arucas',
+                          'Firgas',
+                          'Gáldar',
+                          'Ingenio',
+                          'La Aldea de San Nicolás',
+                          'Mogán',
+                          'Moya',
+                          'San Bartolomé de Tirajana',
+                          'Santa Brígida',
+                          'Santa Lucía de Tirajana',
+                          'Santa María de Guía',
+                          'Tejeda',
+                          'Teror',
+                          'Valleseco',
+                        ]"
+                        label="Area of Preference"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-textarea
+                        v-model="editProfile.aboutMe"
+                        label="About Me"
+                        auto-grow
+                        rows="2"
+                        clearable
+                      ></v-textarea>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog = false"
+                  >Close</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="updateProfile"
+                  >Update</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+      </div>
+
+      <v-list-item>
+        <v-avatar>
+          <img :src="user.photo" :alt="user.name" />
+        </v-avatar>
+        <v-list-item-content class="ml-4">
+          <v-list-item-title class="py-0">
+            {{ user.username }}
+          </v-list-item-title>
+          <v-list-item-title class="py-0"
+            >{{ user.name }} {{ user.lastName }}</v-list-item-title
+          >
+          <v-list-item-subtitle class="py-0 my-0">
+            Rating: {{ user.rating }} | {{ user.email }} |
+            {{ user.areaPreference }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-text>
+        <v-list-item-title class="py-0 title">
+          About Me
         </v-list-item-title>
-        <v-list-item-title class="py-0"
-          >{{ user.name }} {{ user.lastName }}</v-list-item-title
-        >
-        <v-list-item-subtitle class="py-0 my-0">
-          Rating: {{ user.rating }} | {{ user.email }} |
-          {{ user.areaPreference }}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+        <div>{{ user.aboutMe }}</div>
+      </v-card-text>
 
-    <v-card-text>
-      <v-list-item-title class="py-0 title">
-        About Me
-      </v-list-item-title>
-      <div>{{ user.aboutMe }}</div>
-    </v-card-text>
+      <v-divider class="mx-4"></v-divider>
 
-    <v-divider class="mx-4"></v-divider>
+      <div class="hidden-sm-and-up">
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          My Events -
+          <v-btn :to="'/users/me/myEvents'" class="pl-0" text color="primary">
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCarousel :userevent="user.myEvents" />
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          Attending Events -
+          <v-btn
+            :to="'/users/me/attendingEvents'"
+            class="pl-0"
+            text
+            color="primary"
+          >
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCarousel :userevent="user.attendingEvents" />
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          Saved Events -
+          <v-btn
+            :to="'/users/me/savedEvents'"
+            class="pl-0"
+            text
+            color="primary"
+          >
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCarousel :userevent="user.savedEvents" />
+      </div>
 
-    <div class="hidden-sm-and-up">
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        My Events -
-        <v-btn :to="'/users/me/myEvents'" class="pl-0" text color="primary">
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCarousel :userevent="user.myEvents" />
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        Attending Events -
-        <v-btn
-          :to="'/users/me/attendingEvents'"
-          class="pl-0"
-          text
-          color="primary"
-        >
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCarousel :userevent="user.attendingEvents" />
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        Saved Events -
-        <v-btn :to="'/users/me/savedEvents'" class="pl-0" text color="primary">
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCarousel :userevent="user.savedEvents" />
-    </div>
-
-    <div class="hidden-xs-and-down">
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        My Events -
-        <v-btn :to="'/users/me/myEvents'" class="pl-0" text color="primary">
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCards :userevent="user.myEvents" />
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        Attending Events -
-        <v-btn
-          :to="'/users/me/attendingEvents'"
-          class="pl-0"
-          text
-          color="primary"
-        >
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCards :userevent="user.attendingEvents" />
-      <v-list-item-title class="py-2 pl-4 mt-6">
-        Saved Events -
-        <v-btn :to="'/users/me/savedEvents'" class="pl-0" text color="primary">
-          See All
-        </v-btn>
-      </v-list-item-title>
-      <eventCards :userevent="user.savedEvents" />
-    </div>
-  </v-card>
+      <div class="hidden-xs-and-down">
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          My Events -
+          <v-btn :to="'/users/me/myEvents'" class="pl-0" text color="primary">
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCards :userevent="user.myEvents" />
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          Attending Events -
+          <v-btn
+            :to="'/users/me/attendingEvents'"
+            class="pl-0"
+            text
+            color="primary"
+          >
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCards :userevent="user.attendingEvents" />
+        <v-list-item-title class="py-2 pl-4 mt-6">
+          Saved Events -
+          <v-btn
+            :to="'/users/me/savedEvents'"
+            class="pl-0"
+            text
+            color="primary"
+          >
+            See All
+          </v-btn>
+        </v-list-item-title>
+        <eventCards :userevent="user.savedEvents" />
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -115,10 +227,54 @@ export default {
         attendingEvents: dbUser.data.attendingEvents,
         id: dbUser.data._id,
       },
+      editProfile: {
+        name: dbUser.data.name,
+        lastName: dbUser.data.lastName,
+        email: dbUser.data.email,
+        areaPreference: dbUser.data.areaPreference,
+        aboutMe: dbUser.data.aboutMe,
+      },
     }
   },
   data() {
-    return {}
+    return {
+      dialog: false,
+    }
+  },
+  methods: {
+    updateProfile() {
+      if (
+        !this.editProfile.name === '' ||
+        !this.editProfile.lastName === '' ||
+        !this.editProfile.email === '' ||
+        !this.editProfile.areaPreference === '' ||
+        !this.editProfile.aboutMe === ''
+      ) {
+        const data = {
+          name: this.editProfile.name,
+          lastName: this.editProfile.lastName,
+          email: this.editProfile.email,
+          areaPreference: this.editProfile.areaPreference,
+          aboutMe: this.editProfile.aboutMe,
+        }
+
+        this.$axios
+          .$put(`/users/me`, data, {
+            headers: { token: localStorage.getItem('token') },
+          })
+          .then((response) => {
+            if (!response.errors) {
+              this.dialog = false
+              location.reload()
+            } else {
+              window.alert('Please fill out the form correctly!')
+            }
+          })
+          .catch((err) => console.error(err))
+      } else {
+        window.alert('Please fill out the form correctly!')
+      }
+    },
   },
 }
 </script>
